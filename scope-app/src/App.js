f51c4132a25385932ec5106297b3c41dff6722ee
Ayan Ahmed <ayanahmed@ayans-mbp.home>
2023-01-22 20:11:51 +0000

    setting up API key

---
+++ scope-app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import axios from "axios";
+
+function App() {
+	const url = `https://api.openweathermap.org/data/2.5/weather?q=london&appid=21d2cad1715d85751c0dddc41d01b75c
+  `;
+
+	return;
+
+	<div></div>;
+}
+
+export default App;
